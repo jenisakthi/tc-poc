69a0f7d64e90ccf304edc407862c179835939c7b
jenisakthi <44342960+jenisakthi@users.noreply.github.com>
2024-02-08 00:08:46 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -6,7 +6,7 @@ module "eks" {
   cluster_version = "1.27"
 
   cluster_endpoint_public_access  = true
-
+  enabled_cluster_log_types = []
   
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
