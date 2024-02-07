7bd7bc5a450ba3bb32771e39efc9ac9c69e0affb
jenisakthi <44342960+jenisakthi@users.noreply.github.com>
2024-02-08 00:27:08 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,63 +1,41 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+resource "aws_iam_role" "demo" {
+  name = "eks-cluster-demo"
 
-  cluster_name    = "poc-cluster-jen"
-  cluster_version = "1.27"
-
-  cluster_endpoint_public_access  = true
-  
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.public_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-     ami_type = "AL2_x86_64"
-  }
-
-  eks_managed_node_groups = {
-
-    green = {
-      use_custom_launch_template = false
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
     }
-  }
-  
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+  ]
+}
+POLICY
 }
 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.demo.name
 }
 
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+variable "cluster_name" {
+  default = "poc-cluster-jen"
+  type = string
+  description = "AWS EKS CLuster Name"
+  nullable = false
 }
 
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.20.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
+resource "aws_eks_cluster" "demo" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.demo.arn
+
+  vpc_config {
+    subnet_ids  = module.vpc.private_subnets
   }
+
+  depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
 }
