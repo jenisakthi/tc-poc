f3f8da518675ed17c671a2105459e662e30cd228
jenisakthi <44342960+jenisakthi@users.noreply.github.com>
2024-02-11 23:04:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,9 @@
-resource "aws_subnet" "pre_exist_vpc" {
-    vpc_id = "vpc-03cdd48617e2dffde"
-    cidr_block = "10.10.10.0/24"
-}
+resource "aws_instance" "example" {
+  ami           = "amazon_linux_2"  # Specify the AMI ID of the desired image
+  instance_type = "t3.medium"      # Specify the instance type
 
-resource "aws_instance" "test_ec2" {
-    ami = "ami-033b95fb8079dc481"
-    instance_type = "t3.medium"
-    subnet_id = subnet-02fbea366b52b939e
+
+  subnet_id              = "subnet-02fbea366b52b939e"  # Specify the subnet ID within the VPC
+
+  # Additional configuration parameters such as key_name, tags, etc. can be specified here
 }
