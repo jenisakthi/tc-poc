b7c950e5b37529248cc6e611bdc32bda1e47d07a
jenisakthi <44342960+jenisakthi@users.noreply.github.com>
2024-02-11 23:32:31 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,3 @@
-resource "aws_instance" "example" {
-  ami           = "amazon_linux_2"  # Specify the AMI ID of the desired image
-  instance_type = "t3.medium"      # Specify the instance type
-
-
-  subnet_id              = "subnet-02fbea366b52b939e"  # Specify the subnet ID within the VPC
-
-  # Additional configuration parameters such as key_name, tags, etc. can be specified here
+ resource "aws_internet_gateway" "poc-tc" {
+  vpc_id = module.vpc.name
 }
