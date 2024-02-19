2d35e7196f9349d5d9775db6d29d18f2d3a92da4
jenisakthi <44342960+jenisakthi@users.noreply.github.com>
2024-02-19 15:41:00 +0530

    Create ecr.tf

---
+++ ecr.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "tc-poc" {
+  name = "tc-poc"
+  image_tag_mutability = "MUTABLE"  
+}
+
+output "repository_url" {
+  value = aws_ecr_repository.my_repo.repository_url
+}
